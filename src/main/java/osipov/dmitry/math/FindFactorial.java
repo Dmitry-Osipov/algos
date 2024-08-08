082a0d7b38efdc4a998f8c87610c8cadaee3c341
Dmitry Osipov <dimaosipov00@gmail.com>
2024-08-08 11:38:05 +0300

    Added factorial

---
+++ src/main/java/osipov/dmitry/math/FindFactorial.java
@@ -0,0 +1,44 @@
+package osipov.dmitry.math;
+
+public class FindFactorial {
+    public static void main(String[] args) {
+        System.out.println(Math.round(factorialByStirling(0)));  // 0
+        System.out.println(Math.round(factorialByStirling(1)));  // 1
+        System.out.println(Math.round(factorialByStirling(2)));  // 2
+        System.out.println(Math.round(factorialByStirling(3)));  // 6
+        System.out.println(Math.round(factorialByStirling(4)));  // 24
+        System.out.println(Math.round(factorialByStirling(5)));  // 118
+        System.out.println(Math.round(factorialByStirling(6)));  // 710
+        System.out.println(Math.round(factorialByStirling(7)));  // 4980
+
+        System.out.println("------------------------------------");
+
+        System.out.println(factorial(0));  // 0
+        System.out.println(factorial(1));  // 1
+        System.out.println(factorial(2));  // 2
+        System.out.println(factorial(3));  // 6
+        System.out.println(factorial(4));  // 24
+        System.out.println(factorial(5));  // 120
+        System.out.println(factorial(6));  // 720
+        System.out.println(factorial(7));  // 5040
+    }
+
+    public static double factorialByStirling(int n) {
+        var firstOperand = Math.sqrt(2 * n * Math.PI);
+        var secondOperand = Math.pow(n, n);
+        var thirdOperand = Math.pow(Math.E, -n);
+        return firstOperand * secondOperand * thirdOperand;
+    }
+
+    public static long factorial(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+}
